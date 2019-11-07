7c576702c73c16b7db050fc07e030ba5fb0acbea
HuydDo <huydinhdo@yahoo.com>
2019-11-07 14:51:03 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,8 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  
+
   def update
     @post = Post.find(params[:id])
     @post.update(title: params[:title], description: params[:description])
